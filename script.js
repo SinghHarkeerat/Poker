11c774dfb2e5d11c9a821c9d4286af34a63d5088
Harkeerat Singh <singhharkeerat110@gmail.com>
2024-11-23 00:02:15 -0800

    update

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Card {
 class Deck {
     constructor() {
         this.suits = ["h", "d", "c", "s"];
-        this.num = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+        this.num = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"];
     }
 
     createDeck() {
@@ -49,22 +49,41 @@ function dealHand() {
     const img1 = hand[0] + "";
     const img2 = hand[1] + "";
     
-    console.log("working 111")
-    loadHand(img1);
-    loadHand(img2);
-    console.log("working 222")
+    console.log("workingawdawd")
+    loadImg(img1);
+    loadImg(img2);
+    console.log("working iijdsfkoj")
     const handDiv = document.getElementById('hand');
-    handDiv.textContent = `${hand.join(', ')}`;
+    // handDiv.textContent = `${hand.join(', ')}`;
 }
 
-function loadHand(i) {
+function loadImg(i) {
     const img = i;
     const num = img.substr(0, 1);
     const suit = img.substr(1, 2);
-    const address = "images/deck/" + num + "-" + suit + ".svg"
-    console.log(address)
+    const address = "/images/deck/" + num + "-" + suit + ".svg";
+    
+    const card = document.createElement('img');
+    card.src = address;
+    const handDiv = document.getElementById('hand');
+    handDiv.appendChild(card);
+}
 
-}   
+function dealTable(){
+    const tableDiv = document.getElementById('table');
+    for (let i = 0; i < 5; i++) {
+        const cardStr = deck.pop(); 
+        const num = cardStr.substr(0, 1);
+        const suit = cardStr.substr(1, 2);
+        const address = "/images/deck/" + num + "-" + suit + ".svg";
+        const card = document.createElement('img');
+        card.src = address;
+        tableDiv.appendChild(card);
+    }
+}
 
 
-window.onload = dealHand;
+window.onload = function(){
+    dealHand();
+    dealTable();
+}
